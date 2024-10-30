409d2cd84b57e7ff906b36bac5e1817b1443364d
HugoNC <hugo.castro@comunix.net.br>
2024-10-30 17:36:04 -0300

    Troca ícone do loading

--- src/app/(dashboard)/builder/[id]/loading.tsx
+++ src/app/(dashboard)/builder/[id]/loading.tsx
@@ -1,9 +1,9 @@
-import { LucidePen } from "lucide-react";
+import { PencilRuler } from "lucide-react";
 
-export default function Loading(){
+export default function Loading() {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <LucidePen className="animate-spin h-12 w-12"/>
+      <PencilRuler className="animate-spin h-12 w-12" />
     </div>
-  )
+  );
 }
